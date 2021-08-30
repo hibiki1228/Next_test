2946f8d12ea016fd0c3c992667b5d10d3e17bc92
hibiki1228 <devkimadore1228@gmail.com>
2021-08-30 21:10:58 +0900

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+// ./pages/_app.js
+import initAuth from '../initAuth' // the module you created above
+
+initAuth()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
